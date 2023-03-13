5bacd229bb953e72d270852c63e42390e32eab2b
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-03-13 11:42:52 -0500

    Print func & generate mines

--- jsms.js
+++ jsms.js
@@ -14,8 +14,8 @@ REPRESENTED AS AN ARRAY FOR USE IN REACT
     // '1' Mine
 // /key
 
-const height = 10;
-const width = 15;
+const height = 6;
+const width = 8;
 
 const checkBoundary = (x, y, modifier = 0) => {
     //returns object of boundary booleans
@@ -34,11 +34,19 @@ const checkBoundary = (x, y, modifier = 0) => {
         bottomRight: (boundaries.right && boundaries.bottom),
         bottomLeft: (boundaries.left && boundaries.bottom),
     };
-    
+
     return {...boundaries, ...corners};
 };
 
-const generateGameBoard = (gameBoard, firstClickX, firstClickY) => {};
+const generateGameBoard = (gameBoard, firstClickX, firstClickY) => {
+    for(let i = 0; i < width; i++){
+        const thisColumn = [];
+        for(let j = 0; j < height; j++){
+            thisColumn.push(Math.random() >= 0.72 ? 1 : 0);
+        }
+        gameBoard.push(thisColumn);
+    }
+};
 
 const leftClick = () => {};
 
@@ -50,10 +58,19 @@ const regionClear = () => {};
 
 
 
-const printBoard = (board) => {};
+const printBoard = (board) => {
+    for(let j = height - 1; j >= 0; j--){
+        const temp = [];
+        for(let i = 0; i < width; i++){
+            temp.push(board[i][j]);
+        }
+        console.log(temp.join());
+    }
+};
 
 const gameBoard = [];
 const userBoard = [];
+generateGameBoard(gameBoard);
 printBoard(gameBoard);
-printBoard(userBoard);
+// printBoard(userBoard);
 
