b6419661e4e7203ac85fac361828c02cd95da077
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-03-13 10:55:16 -0500

    App layout

---
+++ jsms.js
@@ -0,0 +1,21 @@
+/*
+JAVASCRIPT MINESWEEPER
+REPRESENTED AS AN ARRAY FOR USE IN REACT
+*/
+
+const height = 10;
+const width = 15;
+
+const generateGameBoard = (gameBoard, firstClickX, firstClickY) => {};
+
+const leftClick = () => {};
+
+const rightClick = () => {};
+
+const popCell = () => {};
+
+const regionClear = () => {};
+
+
+
+const printBoard = (board) => {};
